2ad7e98c5a893d6127966d2a17727225d5c76549
ParulPrasad <59188895+ParulPrasad@users.noreply.github.com>
2019-12-27 16:55:54 +0530

    prime numbers in an array

--- PrimeNumbersInArray.java
+++ PrimeNumbersInArray.java
@@ -3,7 +3,8 @@ package com.deloitte;
 public class PrimeNumbersInArray {
 
 	public static void main(String[] args) {
-	  int []nums= {2,17,33,4,78,1};
+	  int []nums= {2,17,33,4,78};
+	  System.out.print("Prime Numbers are:");
 	  for(int i:nums) {
 		  int count=0;
 		  for(int j=1;j<=i;j++) {
@@ -11,7 +12,7 @@ public class PrimeNumbersInArray {
 				  count++;
 		  }
 		  if(count==2)
-			  System.out.println(i);
+			  System.out.print(i+"  ");
 			  
 	  }
 	  
