ef1b51634615ac4df59ec56ae1e37b5aef287680
ParulPrasad <59188895+ParulPrasad@users.noreply.github.com>
2019-12-27 15:25:20 +0530

    prime numbers present in an array

---
+++ PrimeNumbersInArray.java
@@ -0,0 +1,21 @@
+package com.deloitte;
+
+public class PrimeNumbersInArray {
+
+	public static void main(String[] args) {
+	  int []nums= {2,17,33,4,78,1};
+	  for(int i:nums) {
+		  int count=0;
+		  for(int j=1;j<=i;j++) {
+			  if(i%j==0)
+				  count++;
+		  }
+		  if(count==2)
+			  System.out.println(i);
+			  
+	  }
+	  
+
+	}
+
+}
